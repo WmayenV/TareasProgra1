3e4b637c8ee9c8792def4b9965b2eb32eeff1a15
William Mayen <willysomv101@gmail.com>
2022-02-27 13:38:32 -0600

    calculadora

---
+++ Tarea1Calculadora/calculadora.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float N1, N2;
+    string math=" ";
+    cout << "Introduce el primer numero: " << endl;
+    cin >> N1;
+    cout <<  "Introduce el segundo numero: " << endl;
+    cin >> N2;
+    cout <<  "La suma de los dos numeros es: " << N1+N2 << endl;
+    cout << endl << "La resta de los dos números es: " << N1-N2 << endl;
+    cout << endl << "La multiplicacion de los dos numeros es: " << N1*N2 << endl;
+    cout << endl << "La dvision de los dos numeros es: " << N1/N2 << endl;
+
+
+}
